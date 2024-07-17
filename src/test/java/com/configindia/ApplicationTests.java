ffa03a3b221005cdf0dcafedddb24f02d195ba59
vansh <vanshrangdhal1627@gmail.com>
2024-07-17 15:19:26 +0530

    This My First Commit added some java Files & made some changes

---
+++ src/test/java/com/configindia/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configindia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
